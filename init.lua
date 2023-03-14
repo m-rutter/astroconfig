72b57c0bd447a5b367ecc1a1d2ee4ddff9155216
Michael Rutter <michael.rutter@OVO-F72MD7GY6C.local>
2023-03-14 09:14:42 +0000

    added language packs

--- init.lua
+++ init.lua
@@ -17,6 +17,15 @@ return {
     },
   },
 
+  plugins = {
+    "AstroNvim/astrocommunity",
+    { import = "astrocommunity.pack.rust" },
+    { import = "astrocommunity.pack.typescript" },
+    { import = "astrocommunity.pack.python" },
+    { import = "astrocommunity.pack.json" },
+    { import = "astrocommunity.pack.yaml" },
+  },
+
   -- Set colorscheme to use
   colorscheme = "astrodark",
 
